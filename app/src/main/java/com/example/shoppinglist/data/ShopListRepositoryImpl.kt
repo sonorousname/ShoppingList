574c55c25074740c4d8917ab9d7d12b11f662d16
Vladimir <sonorous_name@mail.ru>
2025-06-05 12:26:39 +0300

    Finish 5 lessons

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -16,7 +16,7 @@ object ShopListRepositoryImpl : ShopListRepository {
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 1000) {
+        for (i in 0 until 10) {
             addShopItem(ShopItem("Name $i", i, Random.nextBoolean()))
         }
     }
