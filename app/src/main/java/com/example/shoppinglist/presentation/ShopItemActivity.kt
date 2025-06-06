f65078d753c1b1b7ad19b708ec3a9171987ff3e0
Vladimir <sonorous_name@mail.ru>
2025-06-06 15:07:48 +0300

    Finished 6 lessons Finish version

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -7,21 +7,17 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem.Companion.UNDEFINED_ID
+import com.example.shoppinglist.presentation.MainActivity
 import com.google.android.material.textfield.TextInputLayout
 
-class ShopItemActivity : AppCompatActivity() {
+class ShopItemActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedListener {
 
-    private lateinit var viewModel: ShopItemViewModel
-
-    private lateinit var tilName: TextInputLayout
-    private lateinit var tilCount: TextInputLayout
-    private lateinit var etName: EditText
-    private lateinit var etCount: EditText
-    private lateinit var buttonSave: Button
     private var shopItemId = UNDEFINED_ID
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,77 +25,19 @@ class ShopItemActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_shop_item)
 
-        initViews()
-        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
-
         shopItemId = intent.getIntExtra(KEY_SHOP_ITEM_ID, UNDEFINED_ID)
-        when (shopItemId) {
-            UNDEFINED_ID -> launchAddMode()
-            else -> launchEditMode()
+
+        if (savedInstanceState == null) {
+            val fragment = ShopItemFragment.newInstance(shopItemId)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.shop_item_container, fragment)
+                .commit()
         }
 
-        etName.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                viewModel.resetErrorInputName()
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-        })
-
-        etCount.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                viewModel.resetErrorInputName()
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-        })
-
-        viewModel.errorInput.observe(this) {
-            if (it) {
-                tilName.error = "Неправильный ввод"
-                tilCount.error = "Неправильный ввод"
-            } else {
-                tilName.error = null
-                tilCount.error = null
-            }
-        }
-
-        viewModel.shouldCloseScreen.observe(this) {
-            finish()
-        }
     }
 
-    private fun launchEditMode() {
-        viewModel.getShopItem(shopItemId)
-        viewModel.shopItem.observe(this) {
-            etName.setText(it.name)
-            etCount.setText(it.count.toString())
-        }
-        buttonSave.setOnClickListener {
-            viewModel.editShopItem(etName.text.toString(), etCount.text.toString())
-        }
-    }
-
-    private fun launchAddMode() {
-        buttonSave.setOnClickListener {
-            viewModel.addShopItem(etName.text.toString(), etCount.text.toString())
-        }
-    }
-
-    private fun initViews() {
-        tilName = findViewById(R.id.til_name)
-        tilCount = findViewById(R.id.til_count)
-        etName = findViewById(R.id.et_text)
-        etCount = findViewById(R.id.et_count)
-        buttonSave = findViewById(R.id.save_button)
+    override fun onEditingFinished() {
+        finish()
     }
 
     companion object {
---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
@@ -0,0 +1,148 @@
+package com.example.shoppinglist.presentation
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.ViewModelProvider
+import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
+import com.google.android.material.textfield.TextInputLayout
+
+class ShopItemFragment : androidx.fragment.app.Fragment() {
+
+    private lateinit var viewModel: ShopItemViewModel
+
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+    private lateinit var etName: EditText
+    private lateinit var etCount: EditText
+    private lateinit var buttonSave: Button
+    private var shopItemId: Int = UNDEFINED_ID
+
+    private lateinit var onEditingFinishedListener: OnEditingFinishedListener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnEditingFinishedListener) {
+            onEditingFinishedListener = context
+        } else {
+            throw RuntimeException("Activity must implement OnEditingFinishedListener")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_shop_item, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews(view)
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+
+        val args = requireArguments()
+        shopItemId = args.getInt(KEY_SHOP_ITEM_ID, UNDEFINED_ID)
+        when (shopItemId) {
+            UNDEFINED_ID -> launchAddMode()
+            else -> launchEditMode()
+        }
+
+        etName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputName()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        etCount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputName()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        viewModel.errorInput.observe(viewLifecycleOwner) {
+            if (it) {
+                tilName.error = "Неправильный ввод"
+                tilCount.error = "Неправильный ввод"
+            } else {
+                tilName.error = null
+                tilCount.error = null
+            }
+        }
+
+        viewModel.shouldCloseScreen.observe(viewLifecycleOwner) {
+            onEditingFinishedListener.onEditingFinished()
+//            activity?.onBackPressed() //Вместо finish() во фрагменте
+//            finish()
+        }
+    }
+
+
+    private fun launchEditMode() {
+        viewModel.getShopItem(shopItemId)
+        viewModel.shopItem.observe(viewLifecycleOwner) {
+            etName.setText(it.name)
+            etCount.setText(it.count.toString())
+        }
+        buttonSave.setOnClickListener {
+            viewModel.editShopItem(etName.text.toString(), etCount.text.toString())
+        }
+    }
+
+    private fun launchAddMode() {
+        buttonSave.setOnClickListener {
+            viewModel.addShopItem(etName.text.toString(), etCount.text.toString())
+        }
+    }
+
+    private fun initViews(view: View) {
+        tilName = view.findViewById(R.id.til_name)
+        tilCount = view.findViewById(R.id.til_count)
+        etName = view.findViewById(R.id.et_text)
+        etCount = view.findViewById(R.id.et_count)
+        buttonSave = view.findViewById(R.id.save_button)
+    }
+
+    interface OnEditingFinishedListener {
+        fun onEditingFinished()
+    }
+
+    companion object {
+
+        private const val UNDEFINED_ID = -1
+        private const val KEY_SHOP_ITEM_ID = "shopItemId"
+
+        fun newInstance(shopItemId: Int = UNDEFINED_ID): ShopItemFragment {
+            return ShopItemFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(KEY_SHOP_ITEM_ID, shopItemId)
+                }
+            }
+        }
+
+    }
+
+}
